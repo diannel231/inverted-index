c76da9cd9aee1c0b03014cc6cbc9b474a06bc463
sivacharan <siva.mallena@gmail.com>
2022-09-18 01:25:15 -0700

    working merge  and search code

--- index.py
+++ index.py
@@ -9,62 +9,128 @@ class index:
     def __init__(self, path):
         self.path = path
         pass
+    
+    docIdDictionary = {}
+    inverted_index = {}
+    def buildDocIdList(self, doc_files):
+        for idx, each_file in enumerate(doc_files):
+            #print(idx, each_file)
+            self.docIdDictionary[each_file] = idx
+        return self.docIdDictionary
+        
 
     def buildIndex(self):
-
         start_time = time.time()
-
         doc_files = [f for f in os.listdir(self.path) if os.path.isfile(os.path.join(self.path, f))]
-
+        docIdDictionary = self.buildDocIdList(doc_files)
+        
+        #build top down index first with dicId as key
         index = {}
-
         for each_file in doc_files:
-
             with open(self.path + "/" + each_file) as file:
                 text = file.read().replace('\n',' ')
-
             text_array = re.sub('[^a-zA-Z \n]', '', text).lower().split()
-
             token_dictionary = {}
-
             for idx, token in enumerate(text_array):
-
                 if token not in token_dictionary.keys():
-
                     token_dictionary[token] = [idx]
-
                 else:
                     token_dictionary[token].append(idx)
-
-            index[each_file] = token_dictionary
-
-        inverted_index = {}
-
+            index[docIdDictionary[each_file]] = token_dictionary
+        #print(index[0])
+        
+        #build inverted index
         for key, value in index.items():
             doc_id = key
             position_word_index = value
-
             for word, position in position_word_index.items():
+                if word not in self.inverted_index:
+                    self.inverted_index[word] = []
+                self.inverted_index[word].append({doc_id: position})
 
-                if word not in inverted_index:
-
-                    inverted_index[word] = []
-
-                inverted_index[word].append({doc_id: position})
-
+        #sort the posting list
+        for key, value in index.items():
+            self.inverted_index[key] = collections.OrderedDict(sorted(value.items()))
+        
         print("Index built in", time.time() - start_time, "seconds")
+        #print(inverted_index['friedman'])
 
-
-
-
-# def and_query(self, query_terms):
 # function for identifying relevant docs using the index
+    def and_query(self, query_terms):
+        if len(query_terms) == 0:
+            print("Enter atleast one search query!")
+            return
+        if len(query_terms) == 1:
+            return self.getPostingListForTerm(query_terms[0])
+        
+        #more than one search term entered, do AND by intersection
+        result = self.getPostingListForTerm(query_terms[0])
+        for term in query_terms[1:]:
+            result = self.intersect(result, self.getPostingListForTerm(term))
+        self.post_process_results(result, query_terms, True)
+        return result
+    
+    def intersect(self, p1, p2):
+        result = []
+        i = 0
+        j = 0
+        while i < len(p1) and j < len(p2):
+            if p1[i] == p2[j]:
+                result.append(p1[i])
+                i = i + 1
+                j = j + 1
+            elif p1[i] < p2[j]:
+                i = i + 1
+            else:
+                j = j + 1
+        return result
 
-# def print_dict(self):
+# function to process result from AND operation. we get bunch of docIds.
+    def post_process_results(self, result, terms, print_positions):
+        if len(result) == 0:
+            print('No results found!')
+            return
+        print('Total docs retrieved:', len(result))
+        fileNames = list(self.docIdDictionary.keys())
+        docIds = list(self.docIdDictionary.values())
+        for docId in result:
+            fileName = fileNames[docIds.index(docId)]
+            print(fileName)
+            if print_positions == True:
+                for term in terms:
+                    docIdsAndPositions = self.inverted_index[term]
+                    for docIdPosDict in docIdsAndPositions:
+                        docIdInKey = list(docIdPosDict.keys())[0]
+                        if docIdInKey == docId:
+                            positions = docIdPosDict[docIdInKey]
+                            print('Term:"'+term+'" found in file',fileName,'at positions',str(positions))
+                            
+        
+
 # function to print the terms and posting list in the index
+    def print_dict(self):
+        for word, posting_list in self.inverted_index.items():
+            print(word, posting_list)
 
-# def print_doc_list(self):
 # function to print the documents and their document id
+    def print_doc_list(self):
+        for file, idx in self.docIdDictionary.items():
+            print(idx, '==>', file)
+            
+# function to get just the docIds for the term
+    def getPostingListForTerm(self, term):
+        docIds = []
+        if term in self.inverted_index:    
+            posting_list = self.inverted_index[term] #[{0: [0, 46]}, {54: [842]}, {59: [608]}...
+            for docIdPositionDictionary in posting_list:
+                for docId, positions in docIdPositionDictionary.items():
+                    docIds.append(docId)
+        return docIds
+            
 
-a = index("C:/Users/Temp/Documents/GitHub/inverted-index/collection")
+a = index("./collection")
 a.buildIndex()
+a.and_query(['with', 'without', 'yemen', 'yemeni'])
+#print(a.getPostingListForTerm('jordan'))
+#a.print_dict()
+#a.print_doc_list()
